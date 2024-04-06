b91cd20245b853283b3b448765d54b4b34794b4f
YariV2605 <115528654+YariV2605@users.noreply.github.com>
2024-04-06 18:45:30 +0200

    verdergewerkt aan pricingSolver (mip)

--- src/Column.java
+++ src/Column.java
@@ -2,7 +2,7 @@ public class Column {
     private int[][] games;
     private int distance;
 
-    public Column(){}
+    public Column(){}//TODO nodig?
 
     public Column(int[][] games, int distance){//TODO distance berekenen in de constructor i.p.v. via param
         this.games = games;
--- src/PricingSolver.java
+++ src/PricingSolver.java
@@ -2,39 +2,39 @@ import gurobi.*;
 
 public class PricingSolver {
     /**
+     * @param umpire <p>umpire &#x2208 [0, InputManager.getnUmpires()[ --> what umpire to make a column for (wat is the start location)</p>
      *
-     *
-     * @return generated column > d_s
-     * @throws GRBException
+     * @return generated column > d_s or null if no improvements
+     * @throws GRBException gurobi exception
      */
-    public static Column gurobi() throws GRBException {
+    public static Column gurobi(int umpire) throws GRBException {//TODO param voor welke umpire? (dus welke game er de 1ste round gespeeld wordt)
         GRBEnv env = new GRBEnv(true);
-        env.set("logFile", "mip1.log");
+//        env.set("logFile", "mip1.log"); TODO needed?
         env.start();
 
         GRBModel model = new GRBModel(env);
 
 //beslissings Var:
         //komt overeen met a_{i, r, s} uit de paper
-        GRBVar[][] match =new GRBVar[InputManager.getnRounds()][InputManager.getnTeams()];
+        GRBVar[][] a_s =new GRBVar[InputManager.getnRounds()][InputManager.getnTeams()];
         for (int i = 0; i < InputManager.getnTeams(); i++) {
             for (int r = 0; r < InputManager.getnRounds(); r++) {
-                model.addVar(0, 1, 0, GRB.BINARY, "a_{" + i + ", " + r + "}");
+                a_s[i][r] = model.addVar(0, 1, 0, GRB.BINARY, "a_{" + i + ", " + r + "}");
             }
         }
 
-//constraints
+//constraints TODO met de q's, enkel op locaties waar teams thuis spelen, ??1ste locatie vast leggen gebaseerd op input var??
         GRBLinExpr[] constr1LocPerRound = new GRBLinExpr[InputManager.getnRounds()];
         for (int r = 0; r < InputManager.getnRounds(); r++) {
             constr1LocPerRound[r] = new GRBLinExpr();
             int aantalTeamsToRef = 0;
             for (int i = 0; i < InputManager.getnTeams(); i++) {
-                constr1LocPerRound[r].addTerm(1, match[i][r]);
+                constr1LocPerRound[r].addTerm(1, a_s[i][r]);
             }
             model.addConstr(constr1LocPerRound[r], GRB.EQUAL, 1, "refer exactly 1 team in round" + r);
         }
 
-        GRBLinExpr expr = new GRBLinExpr();//TODO iets met de q's
+        GRBLinExpr expr = new GRBLinExpr();
 
 
 //objective
@@ -43,16 +43,22 @@ public class PricingSolver {
         expr_obj.addConstant(v_u);
         for(int i = 0; i < InputManager.getnTeams(); i++){
             for(int r = 0; r < InputManager.getnRounds(); r++){
-                //TODO term: (int) (match[r]==i)/*airs*/ * w_ir
-                expr_obj.addTerm(w_ir, match[i][r]);
+                //TODO w_ir (dual var)
+                expr_obj.addTerm(w_ir, a_s[i][r]);
             }
         }
 
-        model.setObjective(expr_obj, GRB.MAXIMIZE); //maximize --> >d_s
+        model.setObjective(expr_obj, GRB.MAXIMIZE); //maximize --> obj > d_s
         model.update();
-        model.optimize();//TODO enkel een oplossing i.p.v. de optimale?
+        model.optimize();//TODO ??enkel een oplossing i.p.v. de optimale??
 
-        //TODO make column based on gurobi's output
+        //TODO d_s bepalen
+        double d_s = 0.0;//double om te kunnen vergelijken met objective value
+
+        Column column = null;
+        if(model.getObjective(0).getValue() > d_s){
+            column = new Column(/*TODO*/);
+        }
         return column;
     }
 }
