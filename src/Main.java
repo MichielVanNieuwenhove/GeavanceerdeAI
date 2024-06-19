02f6942d8bf28cfcf1b016a1fbdba77685066e8c
YariV2605 <115528654+YariV2605@users.noreply.github.com>
2024-06-19 15:10:44 +0200

    edited for cmd use

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,16 @@ import gurobi.*;
 import java.util.List;
 
 public class Main {
-    private final static String inputFilename = "Instances/umps8C.txt";
-    public static final int q1 = 4;  //An umpire crew must wait q1-1 rounds before revisiting a team's home {0-nUmpires}
-    public static final int q2 = 2;  //An umpire crew must wait q2-1 rounds before officiating the same team again {0-floor(nUmpires/2)}
+    private static String inputFilename = "Instances/umps8C.txt";
+    public static int q1 = 4;  //An umpire crew must wait q1-1 rounds before revisiting a team's home {0-nUmpires}
+    public static int q2 = 2;  //An umpire crew must wait q2-1 rounds before officiating the same team again {0-floor(nUmpires/2)}
 
     public static void main(String[] args) throws GRBException {
+        inputFilename = args[0];
+        q1 = Integer.parseInt(args[1]);
+        q2 = Integer.parseInt(args[2]);
+        boolean useGurobi = Boolean.parseBoolean(args[3]);
+
         InputManager inputManager = new InputManager();
         inputManager.readInput(inputFilename);
         InputManager.print();
@@ -21,7 +26,13 @@ public class Main {
             MasterProblemSolution sol = MasterProblemSolver.gurobi();
             numNewColumns = 0;
             for (int u = 0; u < InputManager.getnUmpires(); u++) {
-                Column c = ColumnGenerator.BAndB(u, sol.getV()[u], sol.getW());
+                Column c;
+                if (useGurobi) {
+                    c = ColumnGenerator.gurobi(u, sol.getV()[u], sol.getW());
+                }
+                else {
+                    c = ColumnGenerator.BAndB(u, sol.getV()[u], sol.getW());
+                }
                 if(c != null) {
                     MasterProblemSolver.addColumn(c, u);
                     numNewColumns++;
